6a9fd949a4f0e232cac1e728b87633299a2863eb
Eyal Zarchi <eyal@zarchi.net>
2024-07-19 14:10:42 +0100

    Update netbird_client_windows.ps1
    changed version to 0.28.5
    removed ps1 script for netbird-ui as 0.28.5 is now opens once.
    
    added message on desktop when client is being upgraded or reinstalled

--- netbird_client_windows.ps1
+++ netbird_client_windows.ps1
@@ -1,11 +1,26 @@
 
-
 # Define the required version
-$requiredVersion = "0.28.3"
+$requiredVersion = "0.28.5"
 $minimalVersion = [version]"0.28.1"
-$netbird_domain = "your netbird domain"
+$netbird_domain = "remote.qwilt.com"
 $netbird_device_port = "33073"
 $netbird_web_port = "443"
+$netbird_ip = "35.246.201.207"
+
+
+# Updating host file for remote.qwilt.com
+$hostsPath = "C:\Windows\System32\drivers\etc\hosts"
+# Read the hosts file
+$hostsContent = Get-Content $hostsPath
+# Remove existing lines containing remote.qwilt.com
+$hostsContent = $hostsContent | Where-Object { $_ -notmatch "$netbird_domain" }
+# Add the new entry
+$newEntry = "$netbird_ip $netbird_domain"
+$hostsContent += $newEntry
+# Write back to the hosts file
+sleep 2
+$hostsContent | Set-Content $hostsPath -Force
+Write-Output "Updated hosts file with: $newEntry"
 
 # Disable ipv6
 Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
@@ -16,6 +31,7 @@ function InstallNetbird {
     echo Installing/Upgrading netbird
     tskill netbird-ui > $null 2>&1
     Stop-Service -Name "NetBird" > $null 2>&1
+    rm C:\ProgramData\Netbird\config.json  > $null 2>&1
     # Get the ProductCode of Netbird
     $ProductCode = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { $_.DisplayName -eq "Netbird" }).PSChildName
 
@@ -38,18 +54,87 @@ function InstallNetbird {
     Stop-Service -Name "NetBird" > $null 2>&1
     sleep 3
     tskill netbird-ui > $null 2>&1
+    Start-Service -Name "NetBird" > $null 2>&1
+    sleep 5
 
- 
-        Start-Service -Name "NetBird" > $null 2>&1
 }
 
+function configure_netbird_show_icon {
+    #create flip icon script
+    @'
+    [CmdletBinding(DefaultParameterSetName='Flip')]
+param (
+    [Parameter(Mandatory=$false, ParameterSetName='Unhide')]
+    [switch]$Unhide,
 
-function configure_start_script { 
-    Remove-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Session Manager\Environment' -Name NB_ICE_RELAY_ACCEPTANCE_MIN_WAIT_SEC   > $null 2>&1
+    [Parameter(Mandatory=$false, ParameterSetName='Hide')]
+    [switch]$Hide,
 
-    # Display a message
-    echo "Updating config file"
+    [Parameter(Mandatory=$false, ParameterSetName='Flip')]
+    [switch]$Flip,
 
+    [Parameter(Mandatory=$false, ValueFromRemainingArguments=$true)]
+    [string[]]$args
+)
+
+function W11_TrayNotify {
+    param (
+        [Parameter(Mandatory)]
+        [string[]] $AppList,
+
+        [Parameter(Mandatory)]
+        [ValidateSet('Unhide','Hide','Flip')]
+        $Action
+    )
+
+    foreach ($GUID in (Get-ChildItem -Path 'HKCU:\Control Panel\NotifyIconSettings' -Name)) {
+        $ChildPath = "HKCU:\Control Panel\NotifyIconSettings\$($GUID)"
+        $Exec = (Get-ItemProperty -Path $ChildPath -Name ExecutablePath -ErrorAction SilentlyContinue).ExecutablePath
+
+        foreach ($App in $AppList) {
+            if ($Exec -match $App) {
+                switch ($Action) {
+                    'Unhide' { $Promoted = 1 }
+                    'Hide'  { $Promoted = 0 }
+                    'Flip' { $Promoted = (Get-ItemProperty -Path $ChildPath -Name IsPromoted).IsPromoted -bxor 1 }
+                }
+                Set-ItemProperty -Path $ChildPath -Name IsPromoted -Value $Promoted
+            }
+        }
+    }
+}
+
+W11_TrayNotify -AppList @("netbird-ui") -Action Unhide
+'@ | Set-Content -Path "C:\ProgramData\Netbird\show_netbird_icon.ps1" -Encoding UTF8
+
+    #showing all icons in the traybar
+    Set-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer EnableAutoTray 0
+    # Get the currently logged-in user session
+    $currentUserSession = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    # set to run after computer resumes from sleep
+    # Define variables
+    
+    # Set to run after computer resumes from sleep
+    # Define variables
+    $taskName = "Run netbird on Resume"
+    $programPath = "C:\Program Files\Netbird\netbird-ui.exe"
+
+    # Create trigger for system resume event using event subscription
+    $triggerXml = @"
+    <QueryList>
+      <Query Id='0' Path='System'>
+        <Select Path='System'>*[System[Provider[@Name='Microsoft-Windows-Power-Troubleshooter'] and (EventID=1)]]</Select>
+      </Query>
+    </QueryList>
+"@
+
+    # Create the action to start the program
+    $action = New-ScheduledTaskAction -Execute $programPath
+
+    # Register the scheduled task
+    Register-ScheduledTask -TaskName $taskName -Trigger (New-ScheduledTaskTrigger -AtStartup -RandomDelay (New-TimeSpan -Seconds 30)) -Action $action -Description "Runs $programPath on system resume" -User "SYSTEM" -RunLevel Highest -Force
+
+    echo Updating config file
     # Define the new values
     $newManagementURL = "${netbird_domain}:${netbird_device_port}"
     $newWebURL = "${netbird_domain}:${netbird_web_port}"
@@ -59,127 +144,31 @@ function configure_start_script {
 
     # Read the contents of the file
     $configContent = Get-Content -Path $configFilePath -Raw
-
     # Replace the strings in the configuration file
-    $newConfigContent = $configContent  -replace 'app.netbird.io:443', $newWebURL -replace 'api.netbird.io:443', $newManagementURL
+    $newConfigContent = $configContent -replace 'api.wiretrustee.com:443', "${netbird_domain}:${netbird_device_port}" -replace 'app.netbird.io:443', "${netbird_domain}:${netbird_web_port}" -replace 'api.netbird.io:443', "${netbird_domain}:${netbird_device_port}"
 
-    # Generate a random number between 20000 and 30000
-    $randomWgPort = Get-Random -Minimum 20000 -Maximum 30001
+    $newConfigContent | Set-Content -Path $configFilePath
+    Start-Service -Name "NetBird" > $null 2>&1
+    # Get the currently logged-in user session
+    $currentUserSession = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 
-    # Create the replacement string
-    $replacementString = '"WgPort": ' + $randomWgPort + ','
+    # Extract the username from the session information
+    $username = $currentUserSession -replace ".*\\"
 
-    # Replace the WgPort line with the new random number
-    $newConfigContent = $newConfigContent -replace '"WgPort": \d+,', $replacementString
+    # Check if the user is currently logged in
+    if ($username) {
+        Write-Host "User $username is currently logged in."
 
-    # Write the updated content back to the config file
-    Set-Content -Path $configFilePath -Value $newConfigContent
-
-    echo "Config file updated successfully"
-    Select-String -Path "C:\ProgramData\Netbird\config.json" -Pattern "33073"
-
-    #create the custom script to run
-    @'
-@echo off
-netbird status > "%temp%\netbird_status.txt" 2>&1
-
-REM Check if the word "error" appears in the output file
-findstr /C:"error" "%temp%\netbird_status.txt" > NUL
-if "%ERRORLEVEL%"=="0" (
-    REM If error is found, restart NetBird service
-    tskill netbird-ui   > NUL 2>&1
-    netbird service restart  > NUL 2>&1
-    netbird service start  > NUL 2>&1
-)
-REM Define the path to the Netbird executable
-set "programPath=C:\Program Files\Netbird\netbird-ui.exe"
-    REM If the program is not running, start it
-    start "" "%programPath%"
-    :startNetbird
-    tskill netbird-ui
-    start netbird-ui
-'@ | Set-Content -Path "C:\ProgramData\Netbird\netbird-ui-qwilt.bat" -Encoding UTF8
-
-#creating ssh script
-$userName = $env:USERNAME
-
-    #create net netbird shortcut file
-            # Define the path to the shortcut file
-    $shortcutPath = "C:\ProgramData\Microsoft\Windows\Start Menu\NetBird.lnk"
-
-    # Define the target path and arguments
-    $targetPath = "C:\ProgramData\Netbird\netbird-ui-qwilt.bat"
-
-    # Define the start-in directory
-    $startIn = "C:\Program Files\Netbird"
-
-    # Define the icon path
-    $iconPath = "C:\Program Files\Netbird\netbird-ui.exe"
-
-    # Create a WScript Shell object
-    $shell = New-Object -ComObject WScript.Shell
-
-    # Create a shortcut object
-    $shortcut = $shell.CreateShortcut($shortcutPath)
-
-    # Set shortcut properties
-    $shortcut.TargetPath = $targetPath
-    $shortcut.Arguments = $arguments
-    $shortcut.WorkingDirectory = $startIn
-    $shortcut.IconLocation = $iconPath
-    $shortcut.WindowStyle = 7  # 7 corresponds to "Minimized"
-
-    # Save the shortcut
-    $shortcut.Save()
-
-    # set netbird shortcut to run as administrator
-    $bytes = [System.IO.File]::ReadAllBytes("C:\ProgramData\Microsoft\Windows\Start Menu\NetBird.lnk")
-    $bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
-    [System.IO.File]::WriteAllBytes("C:\ProgramData\Microsoft\Windows\Start Menu\NetBird.lnk", $bytes)
-    Copy-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\NetBird.lnk" -Destination "C:\Users\Public\Desktop"
-
-    # Define the registry path
-    $registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
-
-    # Define the item properties
-    $itemName = "netbird"
-    $itemType = "String"  # For REG_EXPAND_SZ, we use "String"
-    $itemData = "C:\Program Files\Netbird\netbird-ui.exe"
-
-    # Remove the existing registry item if it exists
-    if (Get-ItemProperty -Path $registryPath -Name $itemName -ErrorAction SilentlyContinue) {
-        Remove-ItemProperty -Path $registryPath -Name $itemName -ErrorAction SilentlyContinue
-        #Write-Output "Existing registry entry '$itemName' removed."
-    }
-
-    # Add the new registry item
-    New-ItemProperty -Path $registryPath -Name $itemName -PropertyType $itemType -Value $itemData  > $null 2>&1
-
-    #showing all icons in the traybar
-    Set-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer EnableAutoTray 0
-
-    # Step 1: Get the active user that is currently logged in
-    $currentUser = Invoke-Expression -Command "whoami"
-
-    # Step 2: Get the path to the user's desktop using environment variables
-    $desktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory)
-
-    # Step 3: Check if the user's desktop folder exists
-    if (Test-Path -Path $desktopPath -PathType Container) {
-        Set-Location -Path $desktopPath
-        Start-Process -FilePath "C:\ProgramData\Microsoft\Windows\Start Menu\NetBird.lnk"
+        # Start the software as the logged-in user
+        $softwarePath = "C:\Program Files\Netbird\netbird-ui.exe"
+        sleep 2
+        Start-Process -FilePath $softwarePath -Verb RunAs $username -WindowStyle Minimized > $null 2>&1
+        Write-Host "Software started for user $username on their desktop."
     } else {
-        Write-Host "Desktop folder not found for user: $currentUser"
+        Write-Host "No user is currently logged in."
     }
-
-    # Unregister the scheduled task
-    Unregister-ScheduledTask -TaskName "Run netbird on Resume" -Confirm:$false -ErrorAction SilentlyContinue  > $null 2>&1
-    Unregister-ScheduledTask -TaskName "Run netbird on login" -Confirm:$false -ErrorAction SilentlyContinue  > $null 2>&1
 
 }
-
-#[Environment]::SetEnvironmentVariable("NB_ICE_RELAY_ACCEPTANCE_MIN_WAIT_SEC", "5", "Machine")
-Start-Service -Name "NetBird" > $null 2>&1
 
 # Check if Netbird is installed
 if (-not (Get-Command netbird -ErrorAction SilentlyContinue)) {
@@ -188,11 +177,33 @@ if (-not (Get-Command netbird -ErrorAction SilentlyContinue)) {
     # Call the installation function
     InstallNetbird
     sleep 10
-    configure_start_script
+    configure_netbird_show_icon
 
 } else {
+    configure_netbird_show_icon # updating start_script file
     # Capture the output of the netbird status command
-    $netbirdStatus = Invoke-Expression "netbird status -d"
+    $netbirdStatus = " "
+# Function to check Netbird status
+    function Get-NetbirdStatus {
+    $processInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $processInfo.FileName = "netbird"
+    $processInfo.Arguments = "status -d"
+    $processInfo.RedirectStandardOutput = $true
+    $processInfo.RedirectStandardError = $true
+    $processInfo.UseShellExecute = $false
+    $processInfo.CreateNoWindow = $true
+
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $processInfo
+    $process.Start() | Out-Null
+
+    $standardOutput = $process.StandardOutput.ReadToEnd()
+    $standardError = $process.StandardError.ReadToEnd()
+    $process.WaitForExit()
+
+    $netbirdStatus = $standardOutput + $standardError
+    return $netbirdStatus
+}
     $netbirdVersionString = Invoke-Expression "netbird version"
     $netbirdVersion = [version]"$netbirdVersionString"
 
@@ -201,37 +212,56 @@ if (-not (Get-Command netbird -ErrorAction SilentlyContinue)) {
         Write-Host "Netbird version is below the minimal version, stopping service and notifying user."
         # Send notification to all logged-in users
         msg * "Netbird version is too old. Forcing Upgrade of Netbird. Please wait."
+        $popup_subject = "Upgrading Netbird"
+        $popup_message = "Netbird version is too old. Forcing Upgrade of Netbird. Please wait."
+        popup # running function to send popup
+
+
         & 'C:\Program Files\Netbird\netbird.exe' down
         & 'C:\Program Files\Netbird\netbird.exe' service stop
         # Run the installation and configuration functions
         InstallNetbird
         Start-Sleep -Seconds 5
-        Configure-StartScript
         & 'C:\Program Files\Netbird\netbird.exe' service restart
     }
-
     # Check if the output contains "Management: Disconnected" or "YOUR_MANAGEMENT_URL"
+
+    # Get Netbird status
+    $netbirdStatus = Get-NetbirdStatus
+
+    if ($netbirdStatus -match "context deadline exceeded") {
+        Write-Host "Netbird service is shutdown or config Damaged - Regenerating"
+        Stop-Service -Name "NetBird" > $null 2>&1
+        rm C:\ProgramData\Netbird\config.json
+        Start-Service -Name "NetBird" > $null 2>&1
+        & 'C:\Program Files\Netbird\netbird.exe' service restart 
+    }
+
+
     if ($netbirdStatus -match "Management: Disconnected" -or $netbirdStatus -match "YOUR_MANAGEMENT_URL") {
         Write-Host "Netbird is installed but not connected, verifying version and connecting..."
-        
-        # Check if the output matches the required version
+           # Check if the output matches the required version
         if ($netbirdVersion -eq $requiredVersion) {
             Write-Host "Netbird version is $requiredVersion, Ignoring"
-            configure_start_script
-           & 'C:\Program Files\Netbird\netbird.exe' service restart 
-        } else {
+          } else {
             Write-Host "Netbird Current Version is:" $netbirdVersion
             Write-Host "Netbird version is not $requiredVersion - Upgrading/Downgrading"
+            msg * "Netbird version is not the recommended version - upgrading - Please wait."
+
             # Call the installation function
+            Stop-Service -Name "NetBird" > $null 2>&1
+            rm C:\ProgramData\Netbird\config.json  > $null 2>&1
             InstallNetbird
-            sleep 5
-            configure_start_script
             & 'C:\Program Files\Netbird\netbird.exe' service restart
         }
         
     } else {
         Write-Host "Netbird is connected at the moment, ignoring"
-        configure_start_script
 
     }
+    # show netbird-ui icon
+    C:\ProgramData\Netbird\show_netbird_icon.ps1  > $null 2>&1
+
 }
+echo "Host file:"
+Get-Content C:\Windows\System32\drivers\etc\hosts
