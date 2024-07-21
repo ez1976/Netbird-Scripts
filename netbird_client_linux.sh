1a1045352309cd5b8627620cbfd9f9d1f137d95a
Eyal Zarchi <eyal@zarchi.net>
2024-07-21 11:24:51 +0100

    Update netbird_client_linux.sh

--- netbird_client_linux.sh
+++ netbird_client_linux.sh
@@ -1,182 +1,303 @@
-#!/bin/bash
-install=
-requiredVersion="0.28.5"
-minimalVersion="0.28.0"
-netbird_domain="remote.qwilt.com"
-netbird_ip="35.246.201.207"
-netbird_device_port="33073"
-netbird_web_port="443"
-random_port=$((20000 + RANDOM % 10001))
-logged_in_user=$(w | grep -E 'tty|gdm' | awk '{print $1}' | sort --uniq)
-display=$(w | grep -E 'tty|gdm' | awk '{print $3}' | sort --uniq)
-homefolder=$(grep "^$logged_in_user:" /etc/passwd | cut -d: -f6)
-
-sed -i "/$netbird_domain/d" /etc/hosts
-echo "$netbird_ip  $netbird_domain"  | sudo tee -a /etc/hosts > /dev/null
+# Define the required version
+$requiredVersion = "0.28.5"
+$minimalVersion = [version]"0.28.1"
+$netbird_domain = "remote.qwilt.com"
+$netbird_device_port = "33073"
+$netbird_web_port = "443"
+$netbird_ip = "35.246.201.207"
 
 
-if [[ ! $logged_in_user ]]
-then 
-    echo "No Users Logged In"
-else
-    CONFIG_FILE=$homefolder/.ssh/config
-    if [[ ! -d "$homefolder/.ssh" ]]; then su -c "ssh-keygen -t rsa -N '' -f $homefolder/.ssh/id_rsa" $logged_in_user;fi
+# Updating host file for remote.qwilt.com
+$hostsPath = "C:\Windows\System32\drivers\etc\hosts"
+# Read the hosts file
+$hostsContent = Get-Content $hostsPath
+# Remove existing lines containing remote.qwilt.com
+$hostsContent = $hostsContent | Where-Object { $_ -notmatch "$netbird_domain" }
+# Add the new entry
+$newEntry = "$netbird_ip $netbird_domain"
+$hostsContent += $newEntry
+# Write back to the hosts file
+sleep 2
+$hostsContent | Set-Content $hostsPath -Force
+Write-Output "Updated hosts file with: $newEntry"
 
-    if [[ -z "$CONFIG_FILE" ]]
-        then 
-        # Check if "Host *" exists
-        if ! grep -q '^Host \*' "$CONFIG_FILE"; then
-            # If "Host *" does not exist, add it with the desired settings
-            echo -e "Host *\n  ServerAliveInterval 120\n  ServerAliveCountMax 15" >> "$CONFIG_FILE"
-        else
-            # If "Host *" exists, check if the settings are present
-            if ! grep -q 'ServerAliveInterval 120' "$CONFIG_FILE"; then
-                sed -i '/^Host \*/a\ \ ServerAliveInterval 120\n\ \ ServerAliveCountMax 15' "$CONFIG_FILE"
-            fi
-        fi
-        chown $logged_in_user:$logged_in_user $homefolder/.ssh/config
-    else
-        echo 'Host *' > "$CONFIG_FILE"
-        echo '  ServerAliveInterval 120' >> "$CONFIG_FILE"
-        echo '   ServerAliveCountMax 15' >> "$CONFIG_FILE"
-        chown $logged_in_user:$logged_in_user $homefolder/.ssh/config
-    fi
-fi
+# Disable ipv6
+Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
 
-# Define a function to update config.json and create a cloudfront script
-update_config () { 
-  # Update config.json with the new domain and port values
-  sed -i "s|api.netbird.io:443|$netbird_domain:$netbird_device_port|g" /etc/netbird/config.json
-  sed -i "s|app.netbird.io:443|$netbird_domain:$netbird_web_port|g" /etc/netbird/config.json
-  sed -i "s|\"WgPort\".*,|\"WgPort\": $random_port,|g" /etc/netbird/config.json
-  sed -i '/NetworkMonitor/d' /etc/netbird/config.json
-  sed -i '/IFaceBlackList/i\    "NetworkMonitor": true,' /etc/netbird/config.json
-  
+# Define the installation function
+function InstallNetbird {
+    # Add installation logic here
+    echo Installing/Upgrading netbird
+    tskill netbird-ui > $null 2>&1
+    Stop-Service -Name "NetBird" > $null 2>&1
+    rm C:\ProgramData\Netbird\config.json  > $null 2>&1
+    # Get the ProductCode of Netbird
+    $ProductCode = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { $_.DisplayName -eq "Netbird" }).PSChildName
+
+    if ($ProductCode) {
+        # Run the uninstaller
+        Write-Host "Uninstalling Netbird..."
+        Start-Process -FilePath "msiexec.exe" -ArgumentList "/x $ProductCode /qn" -Wait > $null 2>&1
+        Write-Host "Netbird has been uninstalled."
+    } else {
+        Write-Host "Netbird is not installed on this system."
+    }
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    (New-Object System.Net.WebClient).DownloadFile("https://github.com/netbirdio/netbird/releases/download/v${requiredVersion}/netbird_installer_${requiredVersion}_windows_amd64.msi", "$env:TEMP/Netbird.msi")
+
+    cd $env:TEMP
+    Start-Process msiexec.exe -NoNewWindow -ArgumentList "-i Netbird.msi /quiet"
+    Start-Sleep -Seconds 20
+    Stop-Service -Name "NetBird" > $null 2>&1
+    sleep 3
+    tskill netbird-ui > $null 2>&1
+    Start-Service -Name "NetBird" > $null 2>&1
+    sleep 5
+
 }
 
-notify_users () {
+function configure_netbird_show_icon {
+    #create flip icon script
+    @'
+    [CmdletBinding(DefaultParameterSetName='Flip')]
+param (
+    [Parameter(Mandatory=$false, ParameterSetName='Unhide')]
+    [switch]$Unhide,
 
-user_list=($(who | grep -E "\(:[0-9](\.[0-9])*\)" | awk '{print $1 "@" $NF}' | sort -u))
+    [Parameter(Mandatory=$false, ParameterSetName='Hide')]
+    [switch]$Hide,
 
-for user in $user_list; do
-    username=${user%@*}
-    display=${user#*@}
-    dbus=unix:path=/run/user/$(id -u $username)/bus
+    [Parameter(Mandatory=$false, ParameterSetName='Flip')]
+    [switch]$Flip,
 
-    sudo -u $username DISPLAY=${display:1:-1} \
-                      DBUS_SESSION_BUS_ADDRESS=$dbus \
-                      notify-send "Netbird Notice" "$@"
-done
+    [Parameter(Mandatory=$false, ValueFromRemainingArguments=$true)]
+    [string[]]$args
+)
+
+function W11_TrayNotify {
+    param (
+        [Parameter(Mandatory)]
+        [string[]] $AppList,
+
+        [Parameter(Mandatory)]
+        [ValidateSet('Unhide','Hide','Flip')]
+        $Action
+    )
+
+    foreach ($GUID in (Get-ChildItem -Path 'HKCU:\Control Panel\NotifyIconSettings' -Name)) {
+        $ChildPath = "HKCU:\Control Panel\NotifyIconSettings\$($GUID)"
+        $Exec = (Get-ItemProperty -Path $ChildPath -Name ExecutablePath -ErrorAction SilentlyContinue).ExecutablePath
+
+        foreach ($App in $AppList) {
+            if ($Exec -match $App) {
+                switch ($Action) {
+                    'Unhide' { $Promoted = 1 }
+                    'Hide'  { $Promoted = 0 }
+                    'Flip' { $Promoted = (Get-ItemProperty -Path $ChildPath -Name IsPromoted).IsPromoted -bxor 1 }
+                }
+                Set-ItemProperty -Path $ChildPath -Name IsPromoted -Value $Promoted
+            }
+        }
+    }
 }
-run_commands_on_users () {
 
-user_list=($(who | grep -E "\(:[0-9](\.[0-9])*\)" | awk '{print $1 "@" $NF}' | sort -u))
+W11_TrayNotify -AppList @("netbird-ui") -Action Unhide
+'@ | Set-Content -Path "C:\ProgramData\Netbird\show_netbird_icon.ps1" -Encoding UTF8
 
-for user in $user_list; do
-    username=${user%@*}
-    display=${user#*@}
-    dbus=unix:path=/run/user/$(id -u $username)/bus
+    #showing all icons in the traybar
+    Set-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer EnableAutoTray 0
+    # Get the currently logged-in user session
+    $currentUserSession = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    # set to run after computer resumes from sleep
+    # Define variables
+}
+function configure_netbird {
 
-    sudo -u $username DISPLAY=${display:1:-1} \
-                      DBUS_SESSION_BUS_ADDRESS=$dbus \
-                      "$@" &
-done
+    # Set to run after computer resumes from sleep
+    # Define variables
+    $taskName = "Run netbird on Resume"
+    $programPath = "C:\Program Files\Netbird\netbird-ui.exe"
+
+    # Create trigger for system resume event using event subscription
+    $triggerXml = @"
+    <QueryList>
+      <Query Id='0' Path='System'>
+        <Select Path='System'>*[System[Provider[@Name='Microsoft-Windows-Power-Troubleshooter'] and (EventID=1)]]</Select>
+      </Query>
+    </QueryList>
+"@
+
+    # Create the action to start the program
+    $action = New-ScheduledTaskAction -Execute $programPath
+
+    # Register the scheduled task
+    Register-ScheduledTask -TaskName $taskName -Trigger (New-ScheduledTaskTrigger -AtStartup -RandomDelay (New-TimeSpan -Seconds 30)) -Action $action -Description "Runs $programPath on system resume" -User "SYSTEM" -RunLevel Highest -Force
+
+    echo Updating config file
+    # Define the new values
+    $newManagementURL = "${netbird_domain}:${netbird_device_port}"
+    $newWebURL = "${netbird_domain}:${netbird_web_port}"
+
+    # Path to the config.json file
+    $configFilePath = "C:\ProgramData\Netbird\config.json"
+
+    # Read the contents of the file
+    $configContent = Get-Content -Path $configFilePath -Raw
+    # Replace the strings in the configuration file
+    $newConfigContent = $configContent -replace 'api.wiretrustee.com:443', "${netbird_domain}:${netbird_device_port}" -replace 'app.netbird.io:443', "${netbird_domain}:${netbird_web_port}" -replace 'api.netbird.io:443', "${netbird_domain}:${netbird_device_port}"
+
+    $newConfigContent | Set-Content -Path $configFilePath
+    Start-Service -Name "NetBird" > $null 2>&1
+    # Get the currently logged-in user session
+    $currentUserSession = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+
+    # Extract the username from the session information
+    $username = $currentUserSession -replace ".*\\"
+
+    # Check if the user is currently logged in
+    if ($username) {
+        Write-Host "User $username is currently logged in."
+
+        # Start the software as the logged-in user
+        $softwarePath = "C:\Program Files\Netbird\netbird-ui.exe"
+        sleep 2
+        Start-Process -FilePath $softwarePath -Verb RunAs $username -WindowStyle Minimized > $null 2>&1
+        Write-Host "Software started for user $username on their desktop."
+    } else {
+        Write-Host "No user is currently logged in."
+    }
+
 }
 
+# Check if Netbird is installed
+if (-not (Get-Command netbird -ErrorAction SilentlyContinue)) {
+    Write-Host "Netbird is not installed, installing now..."
+    
+    # Call the installation function
+    InstallNetbird
+    sleep 10
+    configure_netbird_show_icon
+    sleep 5
+    configure_netbird
 
-if [[ "$(which /usr/bin/netbird 2>/dev/null | grep -c netbird)" -eq 0 ]]
-then 
-    echo "Netbird is missing, installing it now"
-    install=1
-else
-    if  [[ "$(/usr/bin/netbird status 2>&1 | grep "deadline")" ]]
-    then 
-        echo "Netbird Service is shutdown or config.json is corrupted - regenerating"
-        /usr/bin/netbird service stop  > /dev/null 2>&1
-        rm -f /etc/netbird/config.json  > /dev/null 2>&1
-        /usr/bin/netbird service start  > /dev/null 2>&1
-        sleep 3
-        update_config
-        /usr/bin/netbird service restart
 
-    fi
+} else {
+    configure_netbird_show_icon # updating start_script file
+    # Capture the output of the netbird status command
+    $netbirdStatus = " "
+# Function to check Netbird status
+    function Get-NetbirdStatus {
+    $processInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $processInfo.FileName = "netbird"
+    $processInfo.Arguments = "status -d"
+    $processInfo.RedirectStandardOutput = $true
+    $processInfo.RedirectStandardError = $true
+    $processInfo.UseShellExecute = $false
+    $processInfo.CreateNoWindow = $true
 
-    installedVersion=$(/usr/bin/netbird version)
-    if [[ "$(printf '%s\n' "$minimalVersion" "$installedVersion" | sort -V | head -n1)" == "$minimalVersion" && "$installedVersion" != "$minimalVersion" ]]; then
-        echo "The installed version ($installedVersion) is at least $minimalVersion."
-        service netbird start
-        if  [[ "$(/usr/bin/netbird status 2>/dev/null | grep Management | grep -oc Disconnected )" -ge 1 ]] || [[ "$( /usr/bin/netbird status 2>/dev/null | grep -c YOUR_MANAGEMENT_URL)" -ge 1 ]]
-        then
-            echo "Netbird is disconnected or not installed, checking if $requiredVersion is installed"
-            if [[ "$(netbird version)" == "$requiredVersion" ]]
-            then 
-                echo "Netbird is already at $requiredVersion - ignoring"
-                update_config
-                /usr/bin/netbird service restart
-            else
-                echo "Current version is: $(/usr/bin/netbird version)"
-                echo "Netbird version is not $requiredVersion - Upgrading/Downgrading"
-                notify_users "Netbird version is not $requiredVersion - Upgrading/Downgrading"
-                /usr/bin/netbird service stop  > /dev/null 2>&1
-                killall -9 netbird-ui  > /dev/null 2>&1
-                install=1
-            fi #end of netbird version test
-        else
-            echo "Netbird client is connected at the moment, Ignoring"
-            install=0
-        fi # end of connection test
-    else
-        echo "The installed version ($installedVersion) is less than $minimalVersion."
-        notify_users "Netbird installed version ($installedVersion) is less than $minimalVersion - Upgrading"
-        echo "Forcing Netbird Upgrade"
-             netbird down
-            install=1
-    fi
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $processInfo
+    $process.Start() | Out-Null
 
-fi # end of install and version test
+    $standardOutput = $process.StandardOutput.ReadToEnd()
+    $standardError = $process.StandardError.ReadToEnd()
+    $process.WaitForExit()
 
-if [[ "$install" -eq 1 ]]
-then
-    echo "runnning install"
-    notify_users "Netbird version $requiredVersion is being installed. please wait..."
-    if [[ "$(cat /etc/os-release | grep -ociE 'Fedora|centos|rocky' | sort --uniq)" -ge 1 ]]
-    then
-        echo downloading the client
-        pkg_url=$(curl -sIL -o /dev/null -w '%{url_effective}' https://github.com/netbirdio/netbird/releases/download/v$requiredVersion/netbird_${requiredVersion}_linux_amd64.rpm)
-        curl --silent -o /tmp/netbird.rpm "$pkg_url"
-        rpm -Uvh  /tmp/netbird.rpm
+    $netbirdStatus = $standardOutput + $standardError
+    return $netbirdStatus
+}
+    $netbirdVersionString = Invoke-Expression "netbird version"
+    $netbirdVersion = [version]"$netbirdVersionString"
 
-        echo downloading the netbird-ui
-        pkg_url=$(curl -sIL -o /dev/null -w '%{url_effective}' https://github.com/netbirdio/netbird/releases/download/v$requiredVersion/netbird-ui_${requiredVersion}_linux_amd64.rpm)
-        curl --silent -o /tmp/netbird-ui.rpm "$pkg_url"
-        rpm -Uvh  /tmp/netbird-ui.rpm
-    else
-        apt install -y libnotify-bin
-        pkg_url=$(curl -sIL -o /dev/null -w '%{url_effective}' https://github.com/netbirdio/netbird/releases/download/v$requiredVersion/netbird_${requiredVersion}_linux_amd64.deb)
-        # Download the package file
-        curl --silent -o /tmp/netbird.deb "$pkg_url"
-        pkg_url=$(curl -sIL -o /dev/null -w '%{url_effective}' https://github.com/netbirdio/netbird/releases/download/v$requiredVersion/netbird-ui_${requiredVersion}_linux_amd64.deb)
-        # Download the package file
-        curl --silent -o /tmp/netbird-ui.deb "$pkg_url"
-        dpkg -i /tmp/netbird.deb
-        dpkg -i /tmp/netbird-ui.deb
-        sudo apt-get install libnotify-bin -y
+        # Check if the installed version is below the minimal version
+    if ($netbirdVersion -lt $minimalVersion) {
+        Write-Host "Netbird version is below the minimal version, stopping service and notifying user."
+        # Send notification to all logged-in users
+        msg * "Netbird version is too old. Forcing Upgrade of Netbird. Please wait."
+        $popup_subject = "Upgrading Netbird"
+        $popup_message = "Netbird version is too old. Forcing Upgrade of Netbird. Please wait."
+        popup # running function to send popup
 
-    fi # end of netbird client based on OS version
 
-    /usr/bin/netbird service stop  > /dev/null 2>&1
-    killall -9 netbird-ui  > /dev/null 2>&1
-    update_config # calling function
-    /usr/bin/netbird service install  > /dev/null 2>&1
-    /usr/bin/netbird service start   > /dev/null 2>&1
+        & 'C:\Program Files\Netbird\netbird.exe' down
+        & 'C:\Program Files\Netbird\netbird.exe' service stop
+        # Run the installation and configuration functions
+        InstallNetbird
+        Start-Sleep -Seconds 5
+        & 'C:\Program Files\Netbird\netbird.exe' service restart
+        configure_netbird_show_icon
+        sleep 5
+        configure_netbird
+        }
+    # Check if the output contains "Management: Disconnected" or "YOUR_MANAGEMENT_URL"
 
-fi # finished installing and configuring the client
+    # Get Netbird status
+    $netbirdStatus = Get-NetbirdStatus
 
-run_commands_on_users netbird-ui
+    if ($netbirdStatus -match "context deadline exceeded") {
+        Write-Host "Netbird service is shutdown or config Damaged - Regenerating"
+        Stop-Service -Name "NetBird" > $null 2>&1
+        rm C:\ProgramData\Netbird\config.json
+        Start-Service -Name "NetBird" > $null 2>&1
+        sleep 5
+        configure_netbird_show_icon
+        sleep 5
+        configure_netbird
+        sleep 5
+        & 'C:\Program Files\Netbird\netbird.exe' service restart 
+    }
 
+    # Check if the file contains the string 'netbird.io'
+    if (Test-Path C:\ProgramData\Netbird\config.json) {
+        $configContent = Get-Content C:\ProgramData\Netbird\config.json -Raw
+        if ($configContent -match "netbird.io") {
+            Stop-Service -Name "NetBird" > $null 2>&1
+            rm C:\ProgramData\Netbird\config.json
+            Start-Service -Name "NetBird" > $null 2>&1
+            sleep 5
+            configure_netbird_show_icon
+            sleep 5
+            configure_netbird
+            sleep 5
+            & 'C:\Program Files\Netbird\netbird.exe' service restart 
+        }
+    }
+
+    if ($netbirdStatus -match "Management: Disconnected" -or $netbirdStatus -match "YOUR_MANAGEMENT_URL") {
+        Write-Host "Netbird is installed but not connected, verifying version and connecting..."
+           # Check if the output matches the required version
+        if ($netbirdVersion -eq $requiredVersion) {
+            Write-Host "Netbird version is $requiredVersion, Ignoring"
+          } else {
+            Write-Host "Netbird Current Version is:" $netbirdVersion
+            Write-Host "Netbird version is not $requiredVersion - Upgrading/Downgrading"
+            msg * "Netbird version is not the recommended version - upgrading - Please wait."
+
+            # Call the installation function
+            Stop-Service -Name "NetBird" > $null 2>&1
+            rm C:\ProgramData\Netbird\config.json  > $null 2>&1
+            InstallNetbird
+            sleep 5
+            configure_netbird_show_icon
+            sleep 5
+            configure_netbird
+            sleep 5
+            & 'C:\Program Files\Netbird\netbird.exe' service restart
+        }
+        
+    } else {
+        Write-Host "Netbird is connected at the moment, ignoring"
+
+    }
+    # show netbird-ui icon
+    C:\ProgramData\Netbird\show_netbird_icon.ps1  > $null 2>&1
+
+}
 echo "Host file:"
-cat /etc/hosts | grep $netbird_domain
-echo "config.json Host:"
-sudo cat /etc/netbird/config.json | grep \"Host\":
-echo "SSH Config file:"
-cat $CONFIG_FILE | grep ServerAlive
+Get-Content C:\Windows\System32\drivers\etc\hosts |  Select-String "remote.qwilt.com"
+
+echo "Config File:"
+
+Get-Content C:\ProgramData\Netbird\config.json |  Select-String "remote.qwilt.com"
